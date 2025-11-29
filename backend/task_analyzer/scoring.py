38ee8a3e2d4ebeebc47f3db1446c193350805b4e
Vasu <vasu.singh@bettrhq.com>
2025-11-30 02:19:17 +0530

    feat: adds scoring algo, v1

---
+++ backend/task_analyzer/scoring.py
@@ -0,0 +1,60 @@
+from typing import Any
+
+
+from datetime import date
+import math
+
+def detect_cycle(task, path=None, visited=None): # DFS -> to detect cycles (A->B->A)
+    if path is None:
+        path=set()
+    if visited is None:
+        visited = set()
+    if task.id in path:
+        return True
+    if task.id in visited:
+        return False
+    path.add(task.id)
+    visited.add(task.id)
+
+    for dependency in task.dependencies.all():
+        if detect_cycle(dependency, path, visited):
+            return True
+    path.remove(task.id)
+    return False
+
+def calculate_priority_score(task): # calculating a priority_score based on Urgency, Importance & Effort
+    for dependency in task.dependencies.all():
+        if not dependency.is_completed:
+            return -1.0
+        
+    today = date.today() 
+    days_until_due = (task.due_date - today).days # Urgency Factor
+
+    if days_until_due < 0:
+        # PAST DUE: if a task is 1 day late, it's urgent (100). If it's 5 days late, it's critical (125).
+        # Adding 100 points, to ensure it tops any future tasks
+        urgency_score = 100 + (abs(days_until_due)*5) 
+    elif days_until_due == 0:
+        # DUE TODAY: Maximum Normal Urgency
+        urgency_score = 90
+    else:
+        #FUTURE: Score should be decayed as the date gets further away
+        # Using a decyaing function : 80 / (days + 1)
+        # 1 day away: 40 points. 7 days away: 10 points.
+        urgency_score = 80 / (days_until_due + 1)
+    
+    # Importance (User Preference)
+    importance_score = task.importance * 1.5 # Scale 1-10
+
+    # EFFORT (These are quick wins :D )
+    # The idea is that shorter tasks will help build momentum
+    # So we add a small bonus for tasks that are under 2 hours!
+    effort_score = 0
+    if task.estimated_hours < 2:
+        effort_score = 10
+    elif task.estimated_hours > 8:
+        effort_score = -5
+
+    total_score = urgency_score + importance_score + effort_score
+
+    return round(total_score, 2)
