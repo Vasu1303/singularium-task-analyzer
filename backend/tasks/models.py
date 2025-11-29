5366b547d2ad633be768d635431867182e7332d2
Vasu <vasu.singh@bettrhq.com>
2025-11-30 00:20:05 +0530

    feat: implements Task model

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    due_date = models.DateField()
+    estimated_hours = models.FloatField(
+        validators = [MinValueValidator(0.1)]
+    )
+    importance = models.IntegerField(
+        validators = [MinValueValidator(1), MaxValueValidator(10)],
+        help_text = "1 to 10 (10 being most important)"
+    )
+    dependencies = models.ManyToManyField(
+        'self',
+        symmetrical=False, # If task A depends on B, B doesn't automatically depend on A
+        blank=True,
+        related_name='blocking_tasks'
+    )
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} (Due: {self.due_date})"
